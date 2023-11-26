223573efbe982757f5ce198bda315480bc828cb0
marinho <joaovm048@gmail.com>
2023-11-26 20:59:44 -0300

    quase la

---
+++ bin/exemplo1.js
@@ -0,0 +1,6 @@
+// Exemplo de programa JavaScript válido
+let nome = "João";
+let idade = 25;
+let altura = 1.75;
+
+let mensagem = "Olá, " + nome + "! Você tem " + idade + " anos e mede " + altura + " metros.";
---
+++ src/exemplo1 copy.js
@@ -0,0 +1,6 @@
+// Exemplo de programa JavaScript com erro de gramática
+let nome = "Maria";
+let idade = 30;
+let altura = 1.60;
+
+let mensagem = "Olá, " + nome + "! Você tem " + idade + " anos e mede " + altura + " metros";
