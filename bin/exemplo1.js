21f5e889c7d139ed35240f093da152da9e13c3f4
Arthur <arthurpcristovao@gmail.com>
2023-11-27 08:38:12 -0300

    envio compiladores

--- bin/exemplo1.js
+++
@@ -1,6 +0,0 @@
-// Exemplo de programa JavaScript válido
-let nome = "João";
-let idade = 25;
-let altura = 1.75;
-
-let mensagem = "Olá, " + nome + "! Você tem " + idade + " anos e mede " + altura + " metros.";
--- src/exemplo1 copy.js
+++
@@ -1,6 +0,0 @@
-// Exemplo de programa JavaScript com erro de gramática
-let nome = "Maria";
-let idade = 30;
-let altura = 1.60;
-
-let mensagem = "Olá, " + nome + "! Você tem " + idade + " anos e mede " + altura + " metros";
